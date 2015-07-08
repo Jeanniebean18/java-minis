fe4afaedbfbc4ab3405893ee0ab119ae957476cf
Jeannie <jeannie@kickpunchcreative.com>
2015-07-08 13:27:00 -0500

    modal done

--- global.js
+++ global.js
@@ -1,18 +1,52 @@
-var hamburgerMenu = document.getElementById("hamburger");
-var mobileMenu = document.getElementById("mobile_menu");
+var tab1 = document.getElementById("tab1");
+var content1 = document.getElementById("content1");
 
-hamburgerMenu.onclick = toggle_visibility;
+var tab2 = document.getElementById("tab2");
+var content2 = document.getElementById("content2");
 
-function toggle_visibility() {
+var tab3 = document.getElementById("tab3");
+var content3 = document.getElementById("content3");
+
+tab1.onclick = toggle_display1;
+
+function toggle_display1() {
   
-  if (mobileMenu.style.visibility == "visible") {
-    mobileMenu.style.visibility = "hidden";
+  if (content1.style.display == "block") {
+    content1.style.display = "none";
   }
   else {
-    mobileMenu.style.visibility ="visible";
+    content1.style.display ="block";
+    content2.style.display = "none";
+    content3.style.display = "none";
   }
 }
 
 
+tab2.onclick = toggle_display2;
+
+function toggle_display2() {
+  
+  if (content2.style.display == "block") {
+    content2.style.display = "none";
+  }
+  else {
+    content2.style.display ="block";
+    content1.style.display = "none";
+    content3.style.display = "none";
+  }
+}
 
 
+tab3.onclick = toggle_display3;
+
+function toggle_display3() {
+  
+  if (content3.style.display == "block") {
+    content3.style.display = "none";
+  }
+  else {
+    content3.style.display ="block";
+    content2.style.display = "none";
+    content1.style.display = "none";
+  }
+}
---
+++ modal/global.js
@@ -0,0 +1,17 @@
+var exit = document.getElementById("exit");
+var box = document.getElementById("box");
+var signIn = document.getElementById("sign_in");
+
+signIn.onclick = show_box;
+
+function show_box() {
+  box.style.visibility = "visible";
+}
+
+exit.onclick = hide_box;
+
+function hide_box() {
+  box.style.visibility = "hidden";
+}
+
+
