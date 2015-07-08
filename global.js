70ea27d8f2f933c5ea8d8caee26c4c2a0413b15f
Jeannie <jeannie@kickpunchcreative.com>
2015-07-08 11:14:48 -0500

    barcamp mini menu done.

---
+++ global.js
@@ -0,0 +1,18 @@
+var hamburgerMenu = document.getElementById("hamburger");
+var mobileMenu = document.getElementById("mobile_menu");
+
+hamburgerMenu.onclick = toggle_visibility;
+
+function toggle_visibility() {
+  
+  if (mobileMenu.style.visibility == "visible") {
+    mobileMenu.style.visibility = "hidden";
+  }
+  else {
+    mobileMenu.style.visibility ="visible";
+  }
+}
+
+
+
+
